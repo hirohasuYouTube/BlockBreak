43cc0747ffb1f387f28eee441d3a7c4ff5a8b5b9
Naoyuki Hironaka <naoyuki@hironaka.game>
2020-03-24 18:04:06 +0900

    プロジェクト作成

---
+++ Assets/Ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Rigidbody myRigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = this.GetComponent<Rigidbody>();
+        myRigid.AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if( this.transform.position.x > -4)
+                this.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.transform.position.x < 4)
+                this.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+    }
+}
